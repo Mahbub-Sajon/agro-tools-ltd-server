bcf5cd325d16ee0146ac22a3e64ef678df7bc20e
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-06-03 10:34:47 +0600

    admin role added

--- index.js
+++ index.js
@@ -111,14 +111,22 @@ app.get('/all-users', verifyJWT, async(req, res) => {
     res.send(users);
 });
 
-app.put('/user/admin/:email', async(req, res) => {
+app.put('/user/admin/:email', verifyJWT, async(req, res) => {
     const email = req.params.email;
-    const filter = {email: email};
-    const updateDoc = {
-        $set: {role: 'admin'}, 
-    };
-    const result = await userCollection.updateOne(filter, updateDoc);
-    res.send(result);
+    const requester = req.decoded.email;
+    const requesterAccount = await userCollection.findOne({email: requester});
+    if(requesterAccount.role === 'admin'){
+        const filter = {email: email};
+        const updateDoc = {
+            $set: {role: 'admin'}, 
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+    }
+    else{
+        res.status(403).send({message: 'forbidden'});
+    }
+   
 })
 
 
